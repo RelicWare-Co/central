660d3f56cad10bcbcb039a219ef1674ab048ee2f
Verzach3 <gabrielcorreacardenas@gmail.com>
2026-03-18 10:02:09 -0500

    chore: terraform repo

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,7 +1,7 @@
-import { Outlet, createRootRoute } from '@tanstack/react-router'
-import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
 import { TanStackDevtools } from '@tanstack/react-devtools'
-
+import { createRootRoute, Outlet } from '@tanstack/react-router'
+import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
+import { TooltipProvider } from '#/components/ui/tooltip'
 import '../styles.css'
 
 export const Route = createRootRoute({
@@ -11,7 +11,9 @@ export const Route = createRootRoute({
 function RootComponent() {
   return (
     <>
+    <TooltipProvider>
       <Outlet />
+    </TooltipProvider>
       <TanStackDevtools
         config={{
           position: 'bottom-right',
